43704793bb9df3852e9c64f524dfea52c9c8280a
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-04-26 19:25:19 +0200

    feat: slider

--- src/components/molecules/Slider/Slider.tsx
+++ src/components/molecules/Slider/Slider.tsx
@@ -16,25 +16,40 @@ type Props = {
 
 export const Slider = ({ images }: Props) => {
   const sliderRef = useRef<HTMLUListElement>(null);
+  const slidesCount = images.length;
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const prevSlide = () => {
     console.log("prev");
+    setCurrentSlide((prev) => (prev === 0 ? slidesCount - 1 : prev - 1));
   };
 
   const nextSlide = () => {
     console.log("next");
+    setCurrentSlide((prev) => (prev === slidesCount - 1 ? 0 : prev + 1));
   };
+
+  useEffect(() => {
+    if (sliderRef.current) {
+      const slides = sliderRef.current.querySelectorAll('[data-role="slide"]');
+      const current = slides[currentSlide];
+      console.log(current);
+      if (current instanceof HTMLLIElement)
+        sliderRef.current.scrollTo(current.offsetLeft, 0);
+      console.log(currentSlide);
+    }
+  }, [currentSlide]);
 
   return (
     <div className="relative">
       <ul
-        className="overflow-scroll flex flex-row gap-4 scroll-smooth snap-mandatory snap-x snap-start"
+        className="overflow-hidden flex flex-row gap-4 scroll-smooth snap-x"
         ref={sliderRef}
       >
         {images.map((image, idx) => (
           <li
             key={image.src}
-            className="min-w-[100%] md:min-w-[60%] lg:min-w-[40%] min-h-[300px] bg-blue-500 h-64 flex flex-col items-center justify-center text-white font-bold text-2xl overflow-hidden snap-x snap-start relative"
+            className="min-w-[100%] md:min-w-[60%] lg:min-w-[40%] min-h-[300px] bg-blue-500 h-64 flex flex-col items-center justify-center text-white font-bold text-2xl overflow-hidden snap-start relative"
             data-role="slide"
             data-slide={idx}
           >
@@ -53,7 +68,9 @@ export const Slider = ({ images }: Props) => {
         className={twMerge(
           "w-10 h-10 bg-primary hover:bg-primary-dark focus:bg-primary-dark text-white flex flex-col items-center justify-center rounded-full transition-colors",
           "absolute top-1/2 -translate-y-1/2 left-4 xl:-left-5",
+          "disabled:bg-gray-400",
         )}
+        disabled={currentSlide < 1 || currentSlide === 0}
         onClick={prevSlide}
       >
         <FaAngleLeft />
@@ -63,7 +80,11 @@ export const Slider = ({ images }: Props) => {
         className={twMerge(
           "w-10 h-10 bg-primary hover:bg-primary-dark focus:bg-primary-dark text-white flex flex-col items-center justify-center rounded-full transition-colors",
           "absolute top-1/2 -translate-y-1/2 right-4 xl:-right-5",
+          "disabled:bg-gray-400",
         )}
+        disabled={
+          currentSlide > slidesCount - 1 - 2 || currentSlide === slidesCount - 1
+        }
         onClick={nextSlide}
       >
         <FaAngleLeft className="rotate-180" />
