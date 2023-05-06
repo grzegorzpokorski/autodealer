e18fa964c67d90f972f4822eb49c5bc874942ef5
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-05-06 19:46:50 +0200

    chore: upgrade nextjs to v13.4.1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     remotePatterns: [{ hostname: "media.graphassets.com", protocol: "https" }],
   },
