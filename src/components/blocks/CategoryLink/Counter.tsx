1b564d2f54f7f9617084a2bb8e1ea7bf64e0c4c8
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-05-07 13:00:35 +0200

    feat: add border radius to CategoryLink

--- src/components/blocks/CategoryLink/Counter.tsx
+++ src/components/blocks/CategoryLink/Counter.tsx
@@ -6,6 +6,8 @@ type Props = {
 
 export const Counter = ({ children }: Props) => {
   return (
-    <output className="bg-secondary text-white px-4 py-2">{children}</output>
+    <output className="bg-secondary text-white px-4 py-2 rounded">
+      {children}
+    </output>
   );
 };
