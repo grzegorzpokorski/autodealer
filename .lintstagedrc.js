60b8709b2bf1d20b22ada8ea32c767de5aee5dba
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-05-08 17:33:01 +0200

    fix: fix lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx,md,mdx,graphql,yml,yaml,css,scss,json}": [
-    "pnpm prettier --write",
+  "*.{js,jsx,ts,tsx,md,mdx,graphql,yml,yaml,css,scss,json}": (filenames) => [
+    `pnpm prettier --write ${filenames.join(" ")}`,
   ],
-  "*.{js,jsx,ts,tsx}": [() => "pnpm lint:fix"],
+  ".{js,jsx,ts,tsx}": (filenames) =>
+    `next lint --fix --quiet ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(" --file ")}`,
 };
