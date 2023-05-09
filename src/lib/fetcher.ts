afd1c0b11dae8098b35d4133aa9593fb8db7123a
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-05-09 21:58:38 +0200

    fix: add revalidate arg to fetcher

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -33,7 +33,7 @@ export async function fetcher<Result, Variables>({
       ...(variables && { variables }),
     }),
     cache,
-    // next: { revalidate: 900 },
+    next: { revalidate: 0 },
   });
 
   const body = (await result.json()) as GraphQlErrorRespone<Result>;
