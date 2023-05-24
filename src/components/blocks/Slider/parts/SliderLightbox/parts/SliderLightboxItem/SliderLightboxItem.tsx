44a182b402ac499b98cca1d9ae82671ccdbdd212
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-05-24 20:32:55 +0200

    fix: fix image size in slider lightbox

--- src/components/blocks/Slider/parts/SliderLightbox/parts/SliderLightboxItem/SliderLightboxItem.tsx
+++ src/components/blocks/Slider/parts/SliderLightbox/parts/SliderLightboxItem/SliderLightboxItem.tsx
@@ -10,13 +10,13 @@ export const SliderLightboxItem = ({ image }: Props) => (
     key={image.url}
     className="w-[100dvw] h-[100dvh] flex flex-col items-center justify-center"
   >
-    <div className="max-w-[95dvw] md:max-w-[80dvw] w-[95dvw] md:w-[80dvw] :max-h-[95dvh] h-auto mx-[2.5dvw] md:mx-[10dvw]">
+    <div className="max-w-[95dvw] md:max-w-[80dvw] w-[95dvw] md:w-[80dvw] md:max-h-[95dvh] h-auto mx-[2.5dvw] md:mx-[10dvw]">
       <Image
         src={image.url}
         alt={image.alt || ""}
         height={image.height || 0}
         width={image.width || 0}
-        className="m-auto w-auto h-auto max-h-full"
+        className="max-h-full max-w-full m-auto"
         draggable={false}
         onClick={(e) => e.stopPropagation()}
         placeholder="blur"
