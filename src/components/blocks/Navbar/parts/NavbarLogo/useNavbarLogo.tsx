aa1a89a37ceb69479167cdb0c6bd8f76737f59aa
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-06-11 13:57:32 +0200

    fix: refactor useNavbarLogo hook

--- src/components/blocks/Navbar/parts/NavbarLogo/useNavbarLogo.tsx
+++ src/components/blocks/Navbar/parts/NavbarLogo/useNavbarLogo.tsx
@@ -1,15 +1,8 @@
-import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
 export const useNavbarLogo = () => {
-  const [isHome, setIsHome] = useState(false);
   const pathname = usePathname();
-
-  useEffect(() => {
-    if (pathname === "/") {
-      setIsHome(true);
-    }
-  }, [pathname]);
+  const isHome = pathname === "/" ? true : false;
 
   return { isHome };
 };
