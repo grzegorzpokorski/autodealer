38d38c48fe2b235dbd31f834f48c0fb1e00c97a9
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-06-11 14:22:14 +0200

    feat: add stories for blocks/Footer

---
+++ src/components/blocks/Footer/Footer.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Footer } from "./Footer";
+
+const meta: Meta<typeof Footer> = {
+  title: "Components/blocks/Footer",
+  component: Footer,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Footer>;
+
+export const Default: Story = {
+  args: {},
+};
