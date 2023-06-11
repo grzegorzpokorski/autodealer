3a09c2da8b725a15b05f20cb95eddb932b10bf9a
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-06-11 14:05:05 +0200

    feat: add stories for blocks/Heading

---
+++ src/components/blocks/Heading/Heading.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Heading } from "./Heading";
+
+const meta: Meta<typeof Heading> = {
+  title: "Components/blocks/Heading",
+  component: Heading,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Heading>;
+
+export const H1: Story = {
+  args: {
+    children: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    as: "h1",
+  },
+};
+
+export const H2: Story = {
+  args: {
+    ...H1.args,
+    as: "h2",
+  },
+};
+
+export const H3: Story = {
+  args: {
+    ...H1.args,
+    as: "h3",
+  },
+};
+
+export const H4: Story = {
+  args: {
+    ...H1.args,
+    as: "h4",
+  },
+};
